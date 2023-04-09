e463324a921804f6935ad740e5d2c3bbac71a5ef
patrick2m <pmm12@hotmail.com>
2023-04-08 21:25:28 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+
+  window.location.href = "https://forms.gle/jL8AJ5772gTbNJt27";
+
+  return (
+    <div className='avaliacaoContainer'>
+      <h1>
+        Redirecionando...
+      </h1>
+    </div>
+  );
+}
+
+export default App;
